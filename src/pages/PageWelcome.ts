8d1cded23462954ecefb53057b1e70770e10306b
JasminGloth <jasmin.gloth@dci.education>
2023-07-05 14:10:29 +0200

    html is in

--- src/pages/PageWelcome.ts
+++ src/pages/PageWelcome.ts
@@ -8,21 +8,37 @@ export const PageWelcome = () => {
 		<label for="ship1">Schiff 1</label>
 			<select id="ship1" name="ships">
 				${schiffe.map((ship) => {
-          		return `<option value="${ship.name}">${ship.name}</option>`;})}    			
+          return `<option value="${ship.name}">${ship.name}</option>`;
+        })}    			
   			</select>
   		<label for="ship2">Schiff 2</label>
 		    <select id="ship2" name="ships">
     				${schiffe.map((ship) => {
-              		return `<option value="${ship.name}">${ship.name}</option>`;})}
+              return `<option value="${ship.name}">${ship.name}</option>`;
+            })}
   			</select>
 		<label for="ship3">Schiff 3</label>
 		    <select id="ship3" name="ships">
     				${schiffe.map((ship) => {
-              		return `<option value="${ship.name}">${ship.name}</option>`; })}
+              return `<option value="${ship.name}">${ship.name}</option>`;
+            })}
   			</select>
-		<button class="btnProcess">Auswerten</button>
-		<output></output>
+		<button class="btnProcess">Auswerten</button>		
 	</form>
+	<section>
+		<div class="card">
+			<h2>0</h2>
+			<div class="title">Matrosen</div>
+		</div>
+		<div class="card">
+			<h2>0</h2>
+			<div class="title">Säbel</div>
+		</div>
+		<div class="card">
+			<h2>0</h2>
+			<div class="title">Musketen</div>
+		</div>
+	</section>
 </div>
 `;
 };
