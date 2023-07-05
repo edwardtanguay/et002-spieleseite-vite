e98bff64b0325054a6595b4308cd011316e314ef
JasminGloth <jasmin.gloth@dci.education>
2023-07-05 14:29:11 +0200

    men1-3 finished

--- src/pages/PageWelcome.ts
+++ src/pages/PageWelcome.ts
@@ -52,6 +52,8 @@ export const attachPageWelcomeEvents = () => {
     const schiff2Elem = document.querySelector<HTMLInputElement>("#ship2");
     const schiff3Elem = document.querySelector<HTMLInputElement>("#ship3");
 	const men1 = schiffe.find((m) => m.name === schiff1Elem?.value)?.amount;
-	console.log(men1);
+	const men2 = schiffe.find((m) => m.name === schiff2Elem?.value)?.amount;
+	const men3 = schiffe.find((m) => m.name === schiff3Elem?.value)?.amount;
+	console.log(men1,men2,men3);
   });
 }
