417ad5b4f13eae54161041dea832f8c5430d018e
JasminGloth <jasmin.gloth@dci.education>
2023-07-06 15:44:03 +0200

    bedarfsrechner finished

--- src/pages/PageHandel.ts
+++ src/pages/PageHandel.ts
@@ -4,7 +4,7 @@ export const PageHandel = () => {
   return /*html*/ `
 <div class="PageHandel">
 	<h2 class="überschrift">Handel</h2>
-	<h3>Preisliste normal</h3>
+	<!-- <h3>Preisliste normal</h3>
 	<section class="preisliste">
 		<table>
 			<thead>
@@ -132,7 +132,7 @@ export const PageHandel = () => {
 				</tr>
 			</tbody>
 		</table>
-	</section>
+	</section> -->
 	<h3>Bedarfsrechner</h3>
 	<form action="">
 		<label for="bewohner">Bewohner</label>
@@ -227,30 +227,245 @@ export const attachPageHandelEvents = () => {
     );
 	const _bdrholzElem =
     document.querySelector<HTMLInputElement>(".bdrholz");
-
 	const _bdholz = waren.find((m) => m.name === "Holz")?.bd;
+
+	const _bdrziegelElem = document.querySelector<HTMLInputElement>(".bdrziegel");
+  	const _bdziegel = waren.find((m) => m.name === "Ziegel")?.bd;
+
+	const _bdrweizenElem = document.querySelector<HTMLInputElement>(".bdrweizen");
+  	const _bdweizen = waren.find((m) => m.name === "Weizen")?.bd;
+
+	const _bdrobstElem = document.querySelector<HTMLInputElement>(".bdrobst");
+  	const _bdobst = waren.find((m) => m.name === "Obst")?.bd;
+
+	const _bdrmaisElem = document.querySelector<HTMLInputElement>(".bdrmais");
+    const _bdmais = waren.find((m) => m.name === "Mais")?.bd;
+
+	const _bdrzuckerElem = document.querySelector<HTMLInputElement>(".bdrzucker");
+  	const _bdzucker = waren.find((m) => m.name === "Zucker")?.bd;
+
+	const _bdrhanfElem = document.querySelector<HTMLInputElement>(".bdrhanf");
+  	const _bdhanf = waren.find((m) => m.name === "Hanf")?.bd;
+
+	const _bdrtuchElem = document.querySelector<HTMLInputElement>(".bdrtuch");
+  	const _bdtuch = waren.find((m) => m.name === "Tuch")?.bd;
+
+	const _bdrmetallElem = document.querySelector<HTMLInputElement>(".bdrmetall");
+  	const _bdmetall = waren.find((m) => m.name === "Metall")?.bd;
+
+	const _bdrbaumwolleElem =
+    document.querySelector<HTMLInputElement>(".bdrbaumwolle");
+	const _bdbaumwolle = waren.find((m) => m.name === "Baumwolle")?.bd;
+
+	const _bdrwerkzeugElem =
+    document.querySelector<HTMLInputElement>(".bdrwerkzeug");
+  	const _bdwerkzeug = waren.find((m) => m.name === "Werkzeug")?.bd;
+
+	const _bdrfärbemittelElem =
+    document.querySelector<HTMLInputElement>(".bdrfärbemittel");
+  	const _bdfärbemittel = waren.find((m) => m.name === "Färbemittel")?.bd;
+
+	const _bdrkaffeeElem =
+    document.querySelector<HTMLInputElement>(".bdrkaffee");
+  	const _bdkaffee = waren.find((m) => m.name === "Kaffee")?.bd;
+
+	const _bdrkakaoElem = document.querySelector<HTMLInputElement>(".bdrkakao");
+  	const _bdkakao = waren.find((m) => m.name === "Kakao")?.bd;
+
+	const _bdrtabakElem = document.querySelector<HTMLInputElement>(".bdrtabak");
+  	const _bdtabak = waren.find((m) => m.name === "Tabak")?.bd;
+
+	const _bdrfleischElem =
+    document.querySelector<HTMLInputElement>(".bdrfleisch");
+  	const _bdfleisch = waren.find((m) => m.name === "Fleisch")?.bd;
+
+	const _bdrkleidungElem =
+    document.querySelector<HTMLInputElement>(".bdrkleidung");
+  	const _bdkleidung = waren.find((m) => m.name === "Kleidung")?.bd;
+
+	const _bdrseilElem = document.querySelector<HTMLInputElement>(".bdrseil");
+  	const _bdseil = waren.find((m) => m.name === "Seil")?.bd;
+
+	const _bdrrumElem = document.querySelector<HTMLInputElement>(".bdrrum");
+  	const _bdrum = waren.find((m) => m.name === "Rum")?.bd;
+
+	const _bdrbackwarenElem =
+    document.querySelector<HTMLInputElement>(".bdrbackwaren");
+  	const _bdbackwaren = waren.find((m) => m.name === "Backwaren")?.bd;
 
 
 	if (
-      _bewohneranzahlElem &&
-	  _bdrholzElem &&
-	  _bdholz
-    ) {  
-		const bewohneranzahlElem = _bewohneranzahlElem;
-		const bdrholzElem = _bdrholzElem;
-		const bdholz = _bdholz;		
+    _bewohneranzahlElem &&
+    _bdrholzElem &&
+    _bdholz &&
+    _bdrziegelElem &&
+    _bdziegel &&
+    _bdrweizenElem &&
+    _bdweizen &&
+    _bdrobstElem &&
+    _bdobst &&
+    _bdrmaisElem &&
+    _bdmais &&
+    _bdrzuckerElem &&
+    _bdzucker &&
+    _bdrhanfElem &&
+    _bdhanf &&
+    _bdrtuchElem &&
+    _bdtuch &&
+    _bdrmetallElem &&
+    _bdmetall &&
+    _bdrbaumwolleElem &&
+    _bdbaumwolle &&
+    _bdrwerkzeugElem &&
+    _bdwerkzeug &&
+    _bdrfärbemittelElem &&
+    _bdfärbemittel &&
+    _bdrkaffeeElem &&
+    _bdkaffee &&
+    _bdrkakaoElem &&
+    _bdkakao &&
+    _bdrtabakElem &&
+    _bdtabak &&
+	_bdrfleischElem &&
+	_bdfleisch &&
+	_bdrkleidungElem &&
+	_bdkleidung &&
+	_bdrseilElem &&
+	_bdseil &&
+	_bdrrumElem &&
+	_bdrum &&
+	_bdrbackwarenElem &&
+	_bdbackwaren
+  ) {
+    const bewohneranzahlElem = _bewohneranzahlElem;
 
-		bdrholzElem.innerHTML = String(bdholz);
-	}else {
-      const _errorMessageElem =
-        document.querySelector<HTMLInputElement>(".errorMessage");
-     
-      
+    const bdrholzElem = _bdrholzElem;
+    const bdholz = _bdholz;
+    bdrholzElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdholz)
+    );
 
-      if (_errorMessageElem) {
-        const errorMessageElem = _errorMessageElem;
-        errorMessageElem.innerHTML = "There was an error on the site.";
-      }
+    const bdrziegelElem = _bdrziegelElem;
+    const bdziegel = _bdziegel;
+    bdrziegelElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdziegel)
+    );
+
+    const bdrweizenElem = _bdrweizenElem;
+    const bdweizen = _bdweizen;
+    bdrweizenElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdweizen)
+    );
+
+    const bdrobstElem = _bdrobstElem;
+    const bdobst = _bdobst;
+    bdrobstElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdobst)
+    );
+
+    const bdrmaisElem = _bdrmaisElem;
+    const bdmais = _bdmais;
+    bdrmaisElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdmais)
+    );
+
+    const bdrzuckerElem = _bdrzuckerElem;
+    const bdzucker = _bdzucker;
+    bdrzuckerElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdzucker)
+    );
+
+    const bdrhanfElem = _bdrhanfElem;
+    const bdhanf = _bdhanf;
+    bdrhanfElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdhanf)
+    );
+
+    const bdrtuchElem = _bdrtuchElem;
+    const bdtuch = _bdtuch;
+    bdrtuchElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdtuch)
+    );
+
+    const bdrmetallElem = _bdrmetallElem;
+    const bdmetall = _bdmetall;
+    bdrmetallElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdmetall)
+    );
+
+    const bdrbaumwolleElem = _bdrbaumwolleElem;
+    const bdbaumwolle = _bdbaumwolle;
+    bdrbaumwolleElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdbaumwolle)
+    );
+
+    const bdrwerkzeugElem = _bdrwerkzeugElem;
+    const bdwerkzeug = _bdwerkzeug;
+    bdrwerkzeugElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdwerkzeug)
+    );
+
+    const bdrfärbemittelElem = _bdrfärbemittelElem;
+    const bdfärbemittel = _bdfärbemittel;
+    bdrfärbemittelElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdfärbemittel)
+    );
+
+    const bdrkaffeeElem = _bdrkaffeeElem;
+    const bdkaffee = _bdkaffee;
+    bdrkaffeeElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdkaffee)
+    );
+
+    const bdrkakaoElem = _bdrkakaoElem;
+    const bdkakao = _bdkakao;
+    bdrkakaoElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdkakao)
+    );
+
+	const bdrtabakElem = _bdrtabakElem;
+  	const bdtabak = _bdtabak;
+  	bdrtabakElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdtabak)
+    );
+
+	const bdrfleischElem = _bdrfleischElem;
+  	const bdfleisch = _bdfleisch;
+  	bdrfleischElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdfleisch)
+    );
+
+	const bdrkleidungElem = _bdrkleidungElem;
+  	const bdkleidung = _bdkleidung;
+  	bdrkleidungElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdkleidung)
+    );
+
+	const bdrseilElem = _bdrseilElem;
+  	const bdseil = _bdseil;
+ 	bdrseilElem.innerHTML = String(
+    Math.round(Number(bewohneranzahlElem.value) * bdseil)
+  	);
+
+	const bdrrumElem = _bdrrumElem;
+  	const bdrum = _bdrum;
+  	bdrrumElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdrum)
+    );
+
+	const bdrbackwarenElem = _bdrbackwarenElem;
+  	const bdbackwaren = _bdbackwaren;
+  	bdrbackwarenElem.innerHTML = String(
+      Math.round(Number(bewohneranzahlElem.value) * bdbackwaren)
+    );
+  } else {
+    const _errorMessageElem =
+      document.querySelector<HTMLInputElement>(".errorMessage");
+
+    if (_errorMessageElem) {
+      const errorMessageElem = _errorMessageElem;
+      errorMessageElem.innerHTML = "There was an error on the site.";
     }
+  }
   })
 }
